943f861b89292719f86547ef1974c0a6bef8ad1e
Keaton Freed <keatonafreed2@icloud.com>
2022-03-07 16:05:02 -0800

    added broken guns

--- Assets/Scripts/GunController.cs
+++ Assets/Scripts/GunController.cs
@@ -10,11 +10,14 @@ public class GunController : MonoBehaviour
     public float projectileSpeed = 10f;
     public float lifeTime = 3f;
     public InputActionReference toggleBulletReference;
+    public InputActionReference shootReference;
     private bool bulletColBool = false;
 
     void Start()
     {
-       toggleBulletReference.action.performed += BulletToggle;
+    //    toggleBulletReference.action.performed += BulletToggle;
+       shootReference.action.performed += Shoot;
+       bulletColBool = true;
     }
  
     void Update()
@@ -26,15 +29,17 @@ public class GunController : MonoBehaviour
         bulletColBool = !bulletColBool;
     }
 
-    public void Shoot()
+    public void Shoot(InputAction.CallbackContext obj)
     {
         GameObject projectile = Instantiate(projectilePrefab);
         projectile.GetComponent<BulletController>().colDestroy = bulletColBool;
         projectile.transform.position = projectileSpawnLoc.position;
+        // projectile.transform.position = new Vector3(-5,-1,0);
         Vector3 rotation = projectile.transform.rotation.eulerAngles;
         projectile.transform.rotation = Quaternion.Euler(transform.eulerAngles.x+90, transform.eulerAngles.y, rotation.z);
         projectile.GetComponent<Rigidbody>().AddForce(projectileSpawnLoc.forward * projectileSpeed, ForceMode.Impulse);
         StartCoroutine(DestroyProjectile(projectile,lifeTime));
+        Debug.Log("shot");
     }
  
     private IEnumerator DestroyProjectile(GameObject projectile, float delay)
